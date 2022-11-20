8d4baae7a7ab76bfa8adf6b9647627efaccaf923
Owen Russell-Lanning <owenrusselllanning@gmail.com>
2022-11-19 18:32:17 -0800

    created site login and view site login added

--- web layer/script.js
+++ web layer/script.js
@@ -55,7 +55,7 @@ function init_login_page() {
             }
             else {
                 //store username display create password section
-                window.login = { username: username }
+                login = { username: username }
 
                 document.getElementById("create-password-text").innerHTML = "Welcome " + username;
                 document.getElementById("login-create-user-page").style.visibility = "hidden";
@@ -74,13 +74,13 @@ function init_login_page() {
             return; //no password entered
         }
 
-        window.login.password = password; //set password
+        login.password = password; //set password
 
 
         document.getElementById("login-create-user-password-page").style.visibility = "hidden";
 
         //send to database
-        create_user(window.login.username, window.login.password, receive_api_key);
+        create_user(login.username, login.password, receive_api_key);
 
     }
 
@@ -125,7 +125,7 @@ function init_login_page() {
 function receive_api_key(api_key) {
     //store api key locally and login
     localStorage.setItem("api_key", api_key);
-    localStorage.setItem("login", window.login); //TODO: ENCRYPT LOGIN
+    localStorage.setItem("login", login); //TODO: ENCRYPT LOGIN
 
     //load dashboard as current page now that logged in
     load_dashboard();
@@ -134,10 +134,97 @@ function receive_api_key(api_key) {
 /**
  * loads the dashboard as the main page
  */
-function load_dashboard() {
+async function load_dashboard() {
 
-    //hide all pages
-    document.getElementById("login-page").style.visibility = "hidden";
+    //remove login page
+    document.getElementById("login-page").remove();
+
+    //show dashboard page
+    document.getElementById("dashboard-page").style.visibility = "visible";
+
+    //get the url of the current tab
+    var url = await current_tab();
+    url = new URL(url);
+    document.getElementById("dashboard-site").innerHTML = url.hostname;
+
+    //check if theres a login for the current page
+    get_login(login.username, url.hostname, (is_login) => {
+        var login_indictator = document.getElementById("dashboard-login-indicator");
+        if (!is_login) {
+
+            //display no login page
+            document.getElementById("dashboard-no-login-page").classList.remove("hidden");
+            login_indictator.innerHTML = "No Login";
+            login_indictator.style.backgroundColor = "red";
+
+            //init create login button
+            document.getElementById("dashboard-create-login-btn").onclick = function () {
+                //get values
+                var site_username = document.getElementById("site-login-username-input").value;
+                var site_password = document.getElementById("site-login-password-input").value;
+                if (site_password.trim() == "" || site_username.trim() == "") {
+                    return; //no value in one input
+                }
+
+                this.replaceWith(DEFAULT_LOADING);
+
+
+
+                set_login(login.username, url.hostname, site_username, site_password, (result) => {
+                    document.getElementById("site-login-username-input").value = "";
+                    document.getElementById("site-login-password-input").value = "";
+                    DEFAULT_LOADING.replaceWith(this);
+                    //refresh dashboard
+                    load_dashboard();
+
+                });
+
+            };
+        }
+        else {
+            login_indictator.innerHTML = "Login Saved";
+            login_indictator.style.backgroundColor = "green";
+
+            //display valid login page
+            document.getElementById("dashboard-no-login-page").remove();
+            document.getElementById("dashboard-valid-login-page").classList.remove("hidden");
+
+
+            //populate inputs with login info
+            document.getElementById("site-valid-login-username-input").value = is_login.username;
+
+            document.getElementById("site-valid-login-password-input").value = is_login.password;
+
+            //init unide password button
+            var unhide_pass_btn = document.getElementById("site-valid-login-unhide");
+            unhide_pass_btn.onmousedown = () => { unhide_valid_login_password() };
+            unhide_pass_btn.onmouseup = () => { hide_valid_login_password() };
+
+            //init copy buttons
+            document.getElementById("copy-valid-username").onclick = () => {
+                document.getElementById("site-valid-login-username-input").select();
+                document.execCommand('copy');
+                document.getElementById("site-valid-login-username-input").blur();
+            }
+
+            document.getElementById("copy-valid-password").onclick = () => {
+                var pass_inp = document.getElementById("site-valid-login-password-input");
+                pass_inp.type = "text";
+                pass_inp.select();
+                document.execCommand('copy');
+                pass_inp.blur();
+                pass_inp.type = "password";
+            }
+
+
+        }
+    });
+
+
+
+
+
+
 }
 
 
@@ -186,8 +273,58 @@ function create_user(username, password, callback) {
     callback("key");
 }
 
+/**
+ * gets the login for the given website and username. api key must be in local storage.
+ * results is given to callback. false if no login for website
+ * @param {*} username 
+ * @param {*} website 
+ * @param {*} callback 
+ */
+function get_login(username, website, callback) {
+    var api_key = localStorage.getItem("api_key");
+    callback({ username: "user", password: "pass" });
+}
+
+/**
+ * sets a login for the given website. requires the username, site username and site password. api key must be in local storage
+ * result is given to callback
+ * @param {*} username 
+ * @param {*} wesite 
+ * @param {*} site_username 
+ * @param {*} site_password 
+ */
+function set_login(username, wesite, site_username, site_password, callback) {
+    var api_key = localStorage.getItem("api_key");
+    callback(true);
+}
+
+/**
+ * 
+ * @returns the url of the current tab
+ */
+async function current_tab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let tabs = await chrome.tabs.query(queryOptions);
+    return tabs[0].url;
+}
+
+/**
+ * unhides the valid login password on screen
+ */
+function unhide_valid_login_password() {
+    document.getElementById("site-valid-login-password-input").type = "text";
+}
+
+/**
+ * hides the valid login password on screen
+ */
+function hide_valid_login_password() {
+    document.getElementById("site-valid-login-password-input").type = "password";
+}
+
 //init constansts
 const DEFAULT_LOADING = create_default_loading_img();
+let login = {};
 
 init_login_page();
 
